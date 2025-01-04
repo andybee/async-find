98a2e99d8ea06fa170715c916f9c062e3217385f
Andy Buckingham <andy@andybee.com>
2025-01-04 13:50:45 +0000

    Migrate from node-tap to Node's built-in tester

--- test/index.js
+++ test/index.js
@@ -1,27 +1,34 @@
-const tap = require('tap');
+const assert = require('node:assert');
+const test = require('node:test');
 
 const asyncFind = require('../');
 
-tap.plan(3);
+test('resolves with first resolution', async (t) => {
+  const result = await asyncFind([
+    async () => { throw new Error(); },
+    async () => { throw new Error(); },
+    async () => 1,
+    async () => { throw new Error(); },
+    async () => 2,
+  ]);
 
-asyncFind([
-  async () => { throw new Error(); },
-  async () => { throw new Error(); },
-  async () => 1,
-  async () => { throw new Error(); },
-])
-  .then(result => tap.equal(result, 1));
+  assert.equal(result, 1);
+});
 
-const errorA = new Error();
-const errorB = new Error();
-const errorC = new Error();
-asyncFind([
-  async () => { throw errorA; },
-  async () => { throw errorB; },
-  async () => { throw errorC; },
-])
-  .then(() => tap.fail('Should not resolve'))
-  .catch((error) => {
-    tap.equal(error.code, 'ERR_FUNCTIONS_EXHAUSTED');
-    tap.same(error.internalErrors, [errorA, errorB, errorC]);
-  });
+test('rejects if all rejections', async (t) => {
+  const errorA = new Error();
+  const errorB = new Error();
+  const errorC = new Error();
+
+  await assert.rejects(
+    () => asyncFind([
+      async () => { throw errorA; },
+      async () => { throw errorB; },
+      async () => { throw errorC; },
+    ]),
+    {
+      code: 'ERR_FUNCTIONS_EXHAUSTED',
+      internalErrors: [errorA, errorB, errorC],
+    },
+  );
+});
